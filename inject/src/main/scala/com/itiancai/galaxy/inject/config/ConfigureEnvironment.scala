87b05ea1f74ee77d7749cb1d98aade98e6a0eac1
sanctityQ <qicheng@tftiancai.com>
2016-07-15 17:23:08 +0800

    格式调整

--- inject/src/main/scala/com/itiancai/galaxy/inject/config/ConfigureEnvironment.scala
+++ inject/src/main/scala/com/itiancai/galaxy/inject/config/ConfigureEnvironment.scala
@@ -12,25 +12,25 @@ import scala.reflect.io.{File, Path}
 class ConfigureEnvironment(env: com.twitter.app.Flag[String], configPath: com.twitter.app.Flag[String]) extends StandardEnvironment {
 
 
-  override def customizePropertySources( propertySources: MutablePropertySources) {
+  override def customizePropertySources(propertySources: MutablePropertySources) {
 
 
     super.customizePropertySources(propertySources)
 
     val resourceSourcePath = configPath() + File.separator + env() + File.separator + "**" + File.separator + "*.properties"
 
-//    val resourceSourcePath = this.resolvePlaceholders(configPath());
-    val envPropertySources:MutablePropertySources = this.getPropertySources();
+    //    val resourceSourcePath = this.resolvePlaceholders(configPath());
+    val envPropertySources: MutablePropertySources = this.getPropertySources();
 
-    val resolver:ResourcePatternResolver = new PathMatchingResourcePatternResolver();
+    val resolver: ResourcePatternResolver = new PathMatchingResourcePatternResolver();
     try {
       val resources = resolver.getResources(resourceSourcePath);
-      for (resource <-resources) {
+      for (resource <- resources) {
         envPropertySources.addLast(new ResourcePropertySource(resource));
       }
     } catch {
       case e: IOException => logger.error("load config resource failure, path: [" + resourceSourcePath + "]", e);
-      throw new RuntimeException(e);
+        throw new RuntimeException(e);
     }
     logger.info("load config resource successful, path : [" + resourceSourcePath + "]");
 
