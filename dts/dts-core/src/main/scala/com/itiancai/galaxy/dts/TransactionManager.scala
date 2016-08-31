96c0f7977578abc4d0de5728d3203e099e966ce2
liushengpeng <liushengpeng@tftiancai.com>
2016-08-31 14:48:51 +0800

    TransactionManager 移至core中
    Change-Id: Ice1821c1d8cad9f08ae7729bb30fb5fb159ae5bf

---
+++ dts/dts-core/src/main/scala/com/itiancai/galaxy/dts/TransactionManager.scala
@@ -0,0 +1,13 @@
+package com.itiancai.galaxy.dts
+
+import com.itiancai.galaxy.dts.interceptor.TransactionAttribute
+
+trait TransactionManager {
+
+   def begin(transactionAttribute: TransactionAttribute): TransactionStatus
+
+   def commit(transactionStatus: TransactionStatus)
+
+   def rollback(transactionStatus: TransactionStatus)
+
+}
