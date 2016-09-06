2ff539e6be517b4b52c8765162050c4f56866c74
zhouchangbao <zhouchangbao@tftiancai.com>
2016-09-06 15:14:13 +0800

    dts c_time 默认值调整
    Change-Id: I5e3e73aea7f73c60c55819f70db0a43d511eff33

--- dts/dts-core/src/main/resources/sql/install.sql
+++ dts/dts-core/src/main/resources/sql/install.sql
@@ -13,7 +13,7 @@ CREATE TABLE dts_activity (
   time_out bigint(20) NOT NULL DEFAULT 0 COMMENT '超时时间',
   status tinyint(4) NOT NULL DEFAULT 0 COMMENT '状态: 0-初始,1-pre成功 2-成功,3-失败',
   finish tinyint(4) NOT NULL DEFAULT 0 COMMENT '状态: 0-未开始,1-完成,2-处理中',
-  c_time timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
+  c_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   m_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_tx_id` (`tx_id`)
@@ -26,7 +26,7 @@ CREATE TABLE dts_action (
   instruction_id varchar(63) NOT NULL DEFAULT '' COMMENT '幂等id',
   service_name varchar(63) NOT NULL DEFAULT '' COMMENT '服务名',
   status tinyint(4) NOT NULL DEFAULT 0 COMMENT '0-初始状态,1-prepare状态,2-成功,3-失败',
-  c_time timestamp NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
+  c_time timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   m_time timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_action_id` (`action_id`),
